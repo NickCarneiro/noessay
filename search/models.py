790e4842e949e7b7819d1391d31f919d83d97ef4
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-04 00:44:59 -0600

    change models to make data entry easier

--- search/models.py
+++ search/models.py
@@ -15,8 +15,8 @@ PROFIT_STATUS = (
 )
 
 
-class School(models.Model):
-
+class University(models.Model):
+    homepage_url = models.URLField()
     city = models.CharField(max_length=100)
     state = USStateField()
     name = models.CharField(max_length=200)
@@ -31,30 +31,29 @@ class School(models.Model):
 
 # Create your models here.
 class Scholarship(models.Model):
+    third_party_url = models.URLField()
     title = models.CharField(max_length=400)
     description = models.TextField()
     date_added = models.DateField()
     deadline = models.DateField(blank=True)
-    organization = models.CharField(max_length=200)
-    age_restriction = models.SmallIntegerField(blank=True)
-    state_restriction = USStateField(blank=True)
     essay_required = models.BooleanField()
-    essay_length_words = models.IntegerField()
+    amount_usd = models.IntegerField(blank=True)
+    organization = models.CharField(max_length=200)
+    min_age_restriction = models.SmallIntegerField(blank=True)
+    state_restriction = USStateField(blank=True)
+    essay_length_words = models.IntegerField(blank=True)
     gpa_restriction = models.FloatField(blank=True)
     additional_restriction = models.TextField(blank=True)
     major_restriction = models.CharField(max_length=100, blank=True)
-    college_restriction = models.OneToOneField(School, blank=True)
-    #really should be enum of male, female, other, none
-    gender_restriction = models.SmallIntegerField(choices=GENDER)
-    third_party_url = models.URLField()
-    amount_usd = models.IntegerField(blank=True)
+    university_restriction = models.OneToOneField(University, blank=True)
+    gender_restriction = models.SmallIntegerField(choices=GENDER, blank=True)
     sponsored = models.BooleanField()
 
     def __unicode__(self):
         return self.title
 
 # make models available in admin
-admin.site.register(School)
+admin.site.register(University)
 admin.site.register(Scholarship)
 
 
