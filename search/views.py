4b1e00adbff9d12069303225e38edbe401d291d4
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-06 01:06:34 -0600

    created home page and started serp.

--- search/views.py
+++ search/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render_to_response, redirect, render
+from search.models import Scholarship
 
 def home(request):
     return render_to_response('index.html')
 
+#attempt to render nonexistent file to cause 500 error to make sure logs work
 def error(request):
     return render_to_response('error.html')
+
+def serp(request):
+    scholarships = Scholarship.objects.all()
+    return render_to_response('serp.html', {'scholarship_list': scholarships})
+
