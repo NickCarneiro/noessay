de0cd55f1b0d7def1ded44810063fe304a84cbef
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-21 00:15:28 -0600

    replace XOR id obfuscation with pyDes. encrypted ids become keys which are 64 bit integers

--- search/request_utils.py
+++ search/request_utils.py
@@ -1,7 +1,7 @@
+from pyDes import *
 import base64
-
-secret_key = long(0xD89E452B820C4875)
-chaff = 656538804247
+key = 'redevelo'
+k = des(key, CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
 
 def is_empty(str):
     if str is None:
@@ -10,14 +10,30 @@ def is_empty(str):
         return True
     else:
         return False
+
 # decrypt scholarship key (outputs scholarship id)
 def decrypt_sk(sk):
-    sk_long = long(sk)
-    sid = sk_long ^ secret_key
-    sid_dechaffed = sid / chaff
-    return sid_dechaffed
+    sk_hex = hex(long(sk)).replace('0x', '').replace('L', '')
+    hex_upper = sk_hex.upper()
+    print hex_upper
+    bytes = base64.b16decode(hex_upper)
+    dec_hex = str(k.decrypt(bytes))
+    dec = long(dec_hex, 16)
+    return dec
 
 #encrypt scholarship id (outputs scholarship key)
 def encrypt_sid(sid):
-    encrypted_sid = (sid * chaff) ^ secret_key
-    return str(encrypted_sid)
+    sid_hex = hex(sid).replace('0x', '')
+    enc = k.encrypt(str(sid_hex))
+    encrypted_hex = base64.b16encode(enc)
+    print encrypted_hex
+    return long(encrypted_hex, 16)
+
+def test_encryption():
+    x = 234567890
+    print x
+    y = encrypt_sid(x)
+    print y
+    print decrypt_sk(y)
+
+#test_encryption()
