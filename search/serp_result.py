5cf5bd68888710b8165a631905e1fd5d6e2875a5
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-21 01:14:41 -0600

    more serp styles

--- search/serp_result.py
+++ search/serp_result.py
@@ -3,7 +3,7 @@ class SerpResult:
     def __init__(self, scholarship_key=None, scholarship_model=None):
         self.scholarship_key = scholarship_key
         self.scholarship_model = scholarship_model
-        self.snippet = scholarship_model.description[:300]
+        self.snippet = scholarship_model.description[:250]
         if scholarship_model is not None:
             self.deadline = str(scholarship_model.deadline)
         self.source = scholarship_model.organization
