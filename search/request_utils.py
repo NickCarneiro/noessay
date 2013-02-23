f1681b95e7128d81aaa852ad0b1cb8e037a7e4e4
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-23 13:26:39 -0600

    properly used byte arrays for DES encrypting sids to sks

--- search/request_utils.py
+++ search/request_utils.py
@@ -1,7 +1,8 @@
+import re
 from pyDes import *
 import base64
-key = 'redevelo'
-k = des(key, CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
+secret_key = 'Fitzgeralds remarks abou'
+crypt = triple_des(secret_key, mode=ECB, padmode=PAD_PKCS5)
 
 def is_empty(str):
     if str is None:
@@ -11,26 +12,49 @@ def is_empty(str):
     else:
         return False
 
+def int_to_bytes(data):
+    assert isinstance(data, int), 'input was not an int'
+    byte_array = []
+    for s in 24,16,8,0:
+        byte_array.append(((data >> s) & 0xff))
+    return byte_array
+
+def bytes_to_int(data):
+    assert len(data) == 4, 'must pass in a str/collection of 4 elements'
+    return (
+        (ord(data[0]) & 0xff) << 24 |
+        (ord(data[1]) & 0xff) << 16 |
+        (ord(data[2]) & 0xff) << 8 |
+        (ord(data[3]) & 0xff)
+        )
 # decrypt scholarship key (outputs scholarship id)
 def decrypt_sk(sk):
-    hex_upper = sk.upper()
-    bytes = base64.b16decode(hex_upper)
-    dec_hex = str(k.decrypt(bytes))
-    dec = long(dec_hex, 16)
-    return dec
+
+    assert isinstance(sk, basestring) and len(sk) == 16, (
+        'scholarship key was not a valid string')
+    encrypted_byte_array = []
+    for i in re.findall('..', sk):
+        encrypted_byte_array.append(chr(int(i,16)))
+    bytes = ''.join(encrypted_byte_array)
+    return str(bytes_to_int( crypt.decrypt(bytes) ))
 
 #encrypt scholarship id (outputs scholarship key)
 def encrypt_sid(sid):
-    sid_hex = hex(sid).replace('0x', '')
-    enc = k.encrypt(str(sid_hex))
-    encrypted_hex = base64.b16encode(enc)
-    return encrypted_hex.lower()
+    assert isinstance(sid, basestring), 'scholarship id was not a string'
+    sid_chars = []
+    for ch in int_to_bytes(int(sid)):
+        sid_chars.append(chr(ch))
+    sid_enc = crypt.encrypt(sid_chars)
+    return ''.join("{:02x}".format(ord(c)) for c in sid_enc)
 
 def test_encryption():
-    x = 234567890
+    x = str(1234567890)
     print x
     y = encrypt_sid(x)
     print y
     print decrypt_sk(y)
 
-#test_encryption()
+test_encryption()
+
+
+
