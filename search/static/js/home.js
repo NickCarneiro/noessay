683bc677f65169491fe104545d9012b01c8629c0
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-25 00:03:25 -0600

    added maxmind support for prefilling location on homepage

--- search/static/js/home.js
+++ search/static/js/home.js
@@ -1,3 +1,11 @@
-$(function(){
-   $('#location-combobox').select2({width: 200});
+$(function() {
+    var $locationCombobox = $('#location-combobox');
+    $locationCombobox .select2({width: 200});
+    if (geoip_region) {
+        var stateCode = geoip_region();
+        if ($.type(stateCode) === 'string') {
+            $locationCombobox.val(stateCode).trigger('change');
+        }
+    }
+
 });
