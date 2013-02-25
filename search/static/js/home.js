d2bf9bd6f807a39a1cf79b34a4c09cdd3d34058b
burthawk101@gmail.com <burthawk101@gmail.com>
2013-02-25 00:33:04 -0600

    added google analytics

--- search/static/js/home.js
+++ search/static/js/home.js
@@ -1,7 +1,7 @@
 $(function() {
     var $locationCombobox = $('#location-combobox');
     $locationCombobox .select2({width: 200});
-    if (geoip_region) {
+    if (window['geoip_region']) {
         var stateCode = geoip_region();
         if ($.type(stateCode) === 'string') {
             $locationCombobox.val(stateCode).trigger('change');
