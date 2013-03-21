37484ddb284522470bc176578766e88235e2865c
Nick Carneiro <burthawk101@gmail.com>
2013-03-21 05:12:17 +0000

    switched to collectstatics for django 1.5

--- noessay/settings.py
+++ noessay/settings.py
@@ -63,12 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/ne_dev_static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = '/adminstatics/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
