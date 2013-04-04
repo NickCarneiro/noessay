522e8afe39b6f30ce01f461e4af79977fae684e7
Nick Carneiro <burthawk101@gmail.com>
2013-04-03 23:53:18 -0500

    pagination halfway there

--- search/views/serp.py
+++ search/views/serp.py
@@ -10,12 +10,15 @@ from search.elasticsearch_fields import EsFields as es
 
 ELASTICSEARCH_URL = 'noessay.com:9200'
 DESCRIPTION_LENGTH = 300
-
+PAGE_LENGTH = 10
 def serp(request):
     keyword = request.GET.get('q')
     location = request.GET.get('l')
 
     # refine params
+    start = parse_int_param(request.GET.get('start'), 0)
+    if start % PAGE_LENGTH != 0:
+        start = 0
     no_essay_required = parse_boolean_param(request.GET.get('ne'))
     deadline = parse_string_param(request.GET.get('d'), None)
     ethnicity = parse_int_param(request.GET.get('e'), None)
@@ -79,12 +82,12 @@ def serp(request):
                 }
             }
         }
-    search = Search(query)
+    search = Search(query, size=PAGE_LENGTH, start=start)
     search.add_highlight(es.description, fragment_size=300, number_of_fragments=5)
     results = conn.search(search, indices=DEV_INDEX)
+    total_results = results.total
     scholarships = []
     for schol in results:
-        #print scholarship
         sid = schol.django_id
         if schol._meta.highlight:
             schol.description = schol._meta.highlight['description'][0]
@@ -92,12 +95,27 @@ def serp(request):
             schol.description = description_to_snippet(schol.description)
         sk = request_utils.encrypt_sid(str(sid))
         result = SerpResult(sk, schol)
+
         scholarships.append(result)
+    build_pagination_objects(scholarships, total_results)
     return render_to_response('serp.html',
         {'scholarship_list': scholarships,
          'search_request': search_req,
-         'result_count': len(scholarships)
+         'result_count': total_results
         })
 
 def description_to_snippet(desc):
     return desc[:DESCRIPTION_LENGTH].rsplit(' ', 1)[0] + '...'
+
+def build_pagination_objects(scholarships, total_results):
+    max_links = 10
+    pages = total_results / PAGE_LENGTH
+    links = []
+    link_count = min(max_links, pages)
+    current_page = link_count / 2
+    for i in range(link_count):
+        link = {
+            url: '/'
+        }
+        links.add
+
