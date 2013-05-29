96ef6ac86bf0bd029cbf9afd36e9259094b9e243
Nick Carneiro <burthawk101@gmail.com>
2013-05-29 00:44:14 -0500

    extracted finders

--- noessay/settings/settings_dev.py
+++ noessay/settings/settings_dev.py
@@ -77,14 +77,6 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '*wv)4a&amp;wd7cgf1v*8rbq2bhx8u^$a+i6s8l)nlt&amp;q@7%fb5veq'
 
