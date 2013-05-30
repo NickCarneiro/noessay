6e4e77af542099cfc18b3a72fc444487679103c9
Nick Carneiro <burthawk101@gmail.com>
2013-05-29 21:55:19 -0500

    improved homepage buttons on mobile

--- search/static/js/navshell.js
+++ search/static/js/navshell.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var $locationCombobox = $('#location-combobox');
-    $locationCombobox.select2({width: 180});
+    $locationCombobox.select2({width: 210});
     $locationCombobox.val(ne.state).trigger('change');
 
 });
