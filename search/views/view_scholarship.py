27d84161ab3dec6198c6ba9a674bfcd77bcf0b0c
burt <burt@zilker.(none)>
2013-10-13 17:38:49 -0500

    added view scholarship page

--- search/views/view_scholarship.py
+++ search/views/view_scholarship.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from search import request_utils
 from search.models import Scholarship
 def view_scholarship(request):
@@ -6,4 +7,6 @@ def view_scholarship(request):
     scholarship_id = request_utils.decrypt_sk(scholarship_key)
 
     scholarship = Scholarship.objects.get(id=scholarship_id)
-    return render_to_response('view_scholarship.html', {'scholarship_model': scholarship})
+    return render_to_response('view_scholarship.html',
+                              {'scholarship_model': scholarship},
+                              context_instance=RequestContext(request))
