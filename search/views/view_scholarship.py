4591a2865312ffd32dfe67df1e13452a23a39af9
Nick Carneiro <burthawk101@gmail.com>
2013-11-20 21:55:10 -0600

    changed scholarship permalinks to get scholarship key from path instead of url param

--- search/views/view_scholarship.py
+++ search/views/view_scholarship.py
@@ -2,8 +2,11 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from search import request_utils
 from search.models import Scholarship
-def view_scholarship(request):
-    scholarship_key = request.GET.get('sk')
+
+
+def view_scholarship(request, scholarship_key):
+    if not scholarship_key:
+        scholarship_key = request.GET.get('sk')
     scholarship_id = request_utils.decrypt_sk(scholarship_key)
 
     scholarship = Scholarship.objects.get(id=scholarship_id)
