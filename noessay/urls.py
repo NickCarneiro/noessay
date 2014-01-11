4767a682045b83605e8b635db737e132096ddda7
Nick Carneiro <burthawk101@gmail.com>
2014-01-11 17:33:44 -0600

    redirect all routes to homepage

--- noessay/urls.py
+++ noessay/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 admin.autodiscover()
 
@@ -14,9 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^.*$', RedirectView.as_view(url='/', permanent=True)),
     #SEO canonical urls
-
-
 
 )
